4c2043c763bc24a12e2438ea52cb7df5fcf1af0e
Luiz Alberto <albertojiujitsu.alberto@gmail.com>
2024-05-29 16:29:15 -0300

    envio2

---
+++ ConsoleApp.Exercicio02/ConsoleApp.Exercicio02/Classes/Teste.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Exercicio02.Classes
+{
+    public class Teste
+    {
+        public int IdTeste { get; set; }
+         public Teste()
+        {
+        }
+    }
+}
--- ConsoleApp.Exercicio02/ConsoleApp.Exercicio02/Program.cs
+++ ConsoleApp.Exercicio02/ConsoleApp.Exercicio02/Program.cs
@@ -8,9 +8,15 @@ public class Program
         // Display the number of command line arguments.
         Console.WriteLine("AAAA");
         GerenciadorTarefas gerenciador = new GerenciadorTarefas();
-        Categoria todo = new Categoria("TODOS");
+        Categoria todo  = new Categoria("TODOS");
         Categoria doing = new Categoria("DOING");
-        Categoria done = new Categoria("DONE");
+        Categoria done  = new Categoria("DONE") ;
+
+        Console.WriteLine("AAAA");
+        GerenciadorTarefas gerenciador1 = new GerenciadorTarefas();
+        Categoria tod  = new Categoria("TODOS");
+        Categoria dong = new Categoria("DOING");
+        Categoria doe  = new Categoria("DONE") ;
 
         gerenciador.AdicionarTarefa(todo, new TarefaComPrazo("Ir no banco resolver pipino", new DateTime()));
         gerenciador.AdicionarTarefa(doing, new TarefaSimples("Ministrar aula de .NET"));
